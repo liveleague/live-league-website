8feb23ad1ff6ada435fb332a7814a4abac6f0c14
Jack Sellers <jackwsellers@gmail.com>
2019-08-13 12:50:32 +0100

    Links added to homepage.

--- app.py
+++ app.py
@@ -11,3 +11,23 @@ def index():
     return render_template(
         'index.html', standings=standings, upcoming_events=upcoming_events
     )
+
+@app.route('/artist/<slug>')
+def artist(slug):
+    """Artist's public profile."""
+    return render_template('artist.html')
+
+@app.route('/promoter/<slug>')
+def promoter(slug):
+    """Promoter's public profile."""
+    return render_template('promoter.html')
+
+@app.route('/venue/<slug>')
+def venue(slug):
+    """Venue page."""
+    return render_template('venue.html')
+
+@app.route('/<id>')
+def event(id):
+    """Event page."""
+    return render_template('event.html')
